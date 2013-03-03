1189dc8bd7feb99b42d036aa4e48c74bb56b244b
Filip Zalewski <filip.zalewski@gmail.com>
2013-03-02 19:19:22 -0500

    ported bundle update script to bash

---
+++ update_bundles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+vim_bundle_git=(
+        "git://github.com/slack/vim-bufexplorer"
+        "git://github.com/msanders/snipmate.vim.git"
+        "git://github.com/scrooloose/nerdtree.git"
+        "git://github.com/vim-scripts/c.vim"
+)
+
+bundles_dir=./bundles
+
+echo "Cleaning up $bundles_dir"
+rm -rf $bundles_dir/*
+
+for bundle in ${vim_bundle_git[*]}
+do
+        IFS='/' read -ra bundle_url <<< "$bundle"
+        bundle_name=`echo "${bundle_url[${#bundle_url[@]}-1]}" | sed -e 's/.git$//'`
+        echo "Creating directory for $bundle_name"
+        mkdir $bundles_dir/$bundle_name
+        git clone $bundle $bundles_dir/$bundle_name
+        find $bundles_dir/$bundle_name -name ".git" -exec rm -rf {} \;
+done
